883a36facc9d517c3fdcc67b722558652707ab3e
Raddames Tonui <raddamestonui48@gmail.com>
2025-09-05 17:12:52 +0300

    done wityh layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,24 +4,20 @@ import Icon from '../utilities/Icon';
 const Navbar = ()=> {
   return (
     <header >
-                  {/* <div class="header-wrapper">
-
-        <div>
-          <Icon iconName='microphone' className='icon '/>
+      <div className='header-wrapper'>
+        <div className='logo'>
+          <h1>Help Desk - Sky World Limited</h1>
+          <h2>VENDOR</h2>
         </div>
-          <div class="logo">
-              <h1>Help Desk- Sky World Limited</h1>
-              <h2>VENDOR</h2>
-          </div>
-          <div class="icon-search">
-            <Icon iconName="add" className="icon"/>
-            <Icon iconName="search" className="icon"/>
-            <label htmlFor="searchBox"></label>
-            <input type="search" name="searchBox" />
-            <Icon iconName="notification" className="icon"/>
-            <Icon iconName='avatar' className="icon"/>
-          </div>      
-      </div> */}
+        <div className='icon-search'>
+          <Icon iconName='add' />
+          <Icon iconName='search' />
+          <select name="search" id=""></select>
+          <Icon iconName='notification' />
+          <Icon iconName='avatar'/>
+        </div>
+
+      </div>
     </header>
   );
 };
