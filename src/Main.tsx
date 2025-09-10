3131aa6fc9b03f491e4308bfe2213c787f1ad385
Raddames Tonui <raddamestonui48@gmail.com>
2025-09-10 13:24:01 +0300

    resolved description

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import "@fontsource/inter/400.css"; // Regular
-import "@fontsource/inter/600.css"; // Semi-bold
-import "@fontsource/inter/700.css"; // Bold
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css"; 
+import "@fontsource/inter/700.css"; 
 
 
 import './css/index.css'
